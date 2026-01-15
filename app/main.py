e779bbe1ea584e18226dfe504037f7fc1cfcddcd
SIRINE <sirinemasmoud9@gmail.com>
2026-01-15 15:56:30 +0000

    Adding new routes and testing backend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI(title = "I Professional Assistant")
+app = FastAPI(title = "Running the backend")
 
 @app.get("/")
 
 def root():
-    return {"status": "backend is running"}
+    return {"message":"Hello, I am alive!"}
+
+@app.get("/health")
+
+def heath():
+    return {"status": "OK"}
+
+@app.get("/job")
+
+def heath():
+    return {"job": "enployed"}

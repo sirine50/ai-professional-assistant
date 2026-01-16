2457cd68c3a805a69a4fe474c04e64243dc3cd3e
SIRINE <sirinemasmoud9@gmail.com>
2026-01-16 17:00:21 +0000

    unsedrstanding get

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI(title = "Running the backend")
+users = [
+    {"id": 1, "name": "Alice", "age": 22},
+    {"id": 2, "name": "Bob", "age": 25},
+    {"id": 3, "name": "Charlie", "age": 22},
+]
 
 @app.get("/")
 
@@ -12,7 +17,27 @@ def root():
 def heath():
     return {"status": "OK"}
 
-@app.get("/job")
+@app.get("/users")
 
-def heath():
-    return {"job": "enployed"}
+def get_user(age: int | None = None):
+    if age is None:
+        return users
+    
+    return [user for user in users if user["age"] == age]
+
+@app.get("/userss/{user_id}")
+
+def userss(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            return user
+        return {"status": "user not found 404"}
+
+@app.get("/search")
+
+def search(name: str = "Guest", age: int |None = None ):
+    return {
+        "name": name,
+        "age": age,
+        "status": "working"
+    }
